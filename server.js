441548afea85a82c5e259b4464044ef1d5485764
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2025-02-17 11:20:56 -0500

    add post request for bunnies

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
+const bunnies = require('./bunnies.js');
+let idNumber = 6;
+
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('HELLO WORLD!');
 });
 
+app.post('/bunnies', (req, res) => {
+  // console.log(req.body);
+  const { name } = req.body;
+
+  bunnies.push({
+    id: idNumber,
+    name
+  });
+
+  idNumber++;
+
+  res.send(bunnies);
+});
+
+// app.delete('/bunnies/:id', (req, res) => {
+//   // check if the bunny with the given id exists
+//   // remove it from the array
+
+//   res.send(bunnies);
+// });
 
 const PORT = process.env.PORT || 3000; 
 
